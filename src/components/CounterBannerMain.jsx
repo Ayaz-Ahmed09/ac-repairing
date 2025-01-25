6aa0decedb42506dfbc943c6dea13c799e08d4cc
waheed <waheed.jutt2323@gmail.com>
2025-01-25 15:24:49 +0500

    little correction

--- src/components/CounterBannerMain.jsx
+++ src/components/CounterBannerMain.jsx
@@ -3,7 +3,7 @@ import CounterCard from "./sub-components/CounterCard"; // Adjust the import pat
 
 const CounterBanner = () => {
   return (
-    <section className="py-6 bg-gradient-to-r from-red-600 to-black text-white">
+    <section className="py-6 bg-gradient-to-r from-black via-red-600 to-black text-white">
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Clients Counter */}
         <CounterCard
